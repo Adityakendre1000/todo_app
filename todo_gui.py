d98e43bf50da5f2abb080d046ec067c4ffc4213e
Adityakendre1000 <adityakendre1000@gmail.com>
2023-10-17 14:08:55 +0530

    second commit

--- todo_gui.py
+++ todo_gui.py
@@ -1,1 +1,9 @@
-wehibfwiefqf
+import functions
+import PySimpleGUI as sg 
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+
+window=sg.Window('My To-Do App',layout=[[label], [input_box]])
+window.read()
+window.close()
